5d2c9ddb38dd3a7e382812baa7d54aa4a932bbbb
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-06-28 16:52:44 +0300

    sfsfd

--- Apps/DebugApp/DebugApp/DebugViewController.swift
+++ Apps/DebugApp/DebugApp/DebugViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import MapboxMaps
+import CarPlay
 
 /**
  NOTE: This view controller should be used as a scratchpad
@@ -15,6 +16,42 @@ final class DebugViewController: UIViewController {
         super.viewDidLoad()
         mapView = MapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(mapView)
+        view.backgroundColor = .red
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
     }
+
+    func zoomIn() {
+        let cameraOptions = CameraOptions(zoom: mapView.cameraState.zoom + 1)
+        mapView.camera.ease(to: cameraOptions, duration: 1)
+    }
+
+    func zoomOut() {
+        let cameraOptions = CameraOptions(zoom: mapView.cameraState.zoom - 1)
+        mapView.camera.ease(to: cameraOptions, duration: 1)
+    }
+}
+
+@available(iOS 12.0, *)
+extension DebugViewController: CPMapTemplateDelegate {
+    func mapTemplateDidBeginPanGesture(_ mapTemplate: CPMapTemplate) {
+
+    }
+
+    func mapTemplate(_ mapTemplate: CPMapTemplate, didUpdatePanGestureWithTranslation translation: CGPoint, velocity: CGPoint) {
+
+    }
+
+    func mapTemplate(_ mapTemplate: CPMapTemplate, panWith direction: CPMapTemplate.PanDirection) {
+        
+    }
+}
+
+@available(iOS 12.0, *)
+final class Foo: CPMapTemplate {
+    
 }
